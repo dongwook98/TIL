3b5bb71c250469a2e0d537b2e3bfd3e93a06fd60
dongwookkang <119502777+dongwook98@users.noreply.github.com>
2023-02-24 08:49:04 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.js
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.js
@@ -1,3 +1,14 @@
 function solution(num1, num2) {
-    return parseInt(num1 / num2 * 1000)
+    // console.log("Mf(-1.1)", Math.floor(-1.1)); 출력: -2
+    // console.log("parseInt(-1.1)", parseInt(-1.1)); 출력: -1
+    return Math.floor(num1 / num2 * 1000)
+   
+                
+    // 1. parseInt는 원래 문자를 받는 함수이다. string -> number 로 형변환
+    // 2. 10진수 추론을 위해 string 앞 2글자가 Ox나 OX 인지 비교
+    // parseInt는 위 두가지 과정을 더 거쳐야해서 성능이 더 떨어질수도 있다.
 }
+
+// 1. num1을 num2로 나눈다.
+// 2. 나눈 값에 1000을 곱한다.
+// 3. 그 값에 정수부분을 구한다. : Math.floor()
