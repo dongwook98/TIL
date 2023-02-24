109c25c860ae52ffd7acbf03b96b244611e4d98a
dongwookkang <119502777+dongwook98@users.noreply.github.com>
2023-02-24 09:02:56 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.js
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -5,4 +5,13 @@ function solution(num1, num2) {
     } else {
         return -1
     }
+    return answer; // <- 실행 될 가능성이 있나? 
+    // 0퍼센트이다. 
+    // 왜냐하면 return을 만나면 함수가 종료된다.
+    // if문 조건식 안에는 true 아니면 false 두가지 경우가 올 수 있는데 
+    // true일때는 return 1 을 만나서 함수가 종료되고 
+    // false일때는 return -1 을 만나서 함수가 종료되기 때문이다.
 }
+
+// 1. 두수가 같은지 비교 => 비교연산자
+// 2. 비교 결과에 따라 1 , -1 => 조건문
