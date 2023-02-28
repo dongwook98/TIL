42e4e130b526120d931e122767932bd83d52e1ac
dongwookkang <119502777+dongwook98@users.noreply.github.com>
2023-03-01 00:18:51 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.js
+++ 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.js
@@ -1,7 +1,16 @@
 function solution(numbers) {
     let arr = [];
-    for(let i = 0; i<numbers.length; i++) {
-       arr.push(2 *  numbers[i])
+    // for(let i = 0; i<numbers.length; i++) {
+    //    arr.push(2 *  numbers[i])
+    // }
+    let i = 0;
+    while(i < numbers.length) {
+        arr.push(numbers[i] * 2);
+        i++;
     }
     return arr
 }
+// numbers [1,2,3,4,5]
+
+// 1. numbers에서 원소를 꺼내야함
+// 2. 원소를 꺼내 2배를 한 후 새 배열에 넣어준다.
