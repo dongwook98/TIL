998a8efb9bed1d27b6268e9720daa4fdb7e29ed4
dongwookkang <119502777+dongwook98@users.noreply.github.com>
2023-03-01 19:05:30 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
@@ -1,6 +1,10 @@
 function solution(n) {
-    var answer = Math.ceil(n/7);
-    return answer;
-    
-   
+    let pizzaCount = Math.ceil(n/7);
+    return pizzaCount;
 }
+
+// 1명~7명 1판 (1~7)/7 == 0.xx ~ 1 올림 하면 되겠네
+// 8명~14명 2판 (8~14)/7 == 1.xx ~ 2 올림 하면 되겠네
+// 15명~21명 3판 (15~21)/7 == 2.xx ~ 3 올림 하면 되겠네
+// ...
+// n명 n/7 
