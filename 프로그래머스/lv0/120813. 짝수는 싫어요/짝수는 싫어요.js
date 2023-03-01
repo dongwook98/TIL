b9c39adec60702d19ef1bdba2021addbc24fabdc
dongwookkang <119502777+dongwook98@users.noreply.github.com>
2023-03-01 18:01:42 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,18 +1,11 @@
 function solution(n) {
-    // 홀수 : n % 2 === 1
+    let cnt = 0;
     let arr = [];
-   
-    // 1 % 2 === 1
-    // 2 % 2 === 1 인지 비교
-    // 3 % 2 === 1
-    // ...
-    // n % 2 === 1 
-    for(let i=1; i<=n; i++) {
-        if(i % 2 ===1) {
-            arr.push(i)
+    while(cnt <= n) {
+        if(cnt % 2 === 1){
+            arr.push(cnt);
         }
+        cnt++;
     }
-    
-    
     return arr
 }
