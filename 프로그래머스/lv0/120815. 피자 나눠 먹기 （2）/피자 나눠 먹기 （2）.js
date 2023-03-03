785c955d79e4b941b1465f316086be03c8b655a9
dongwookkang <119502777+dongwook98@users.noreply.github.com>
2023-03-03 09:41:43 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
@@ -2,10 +2,11 @@ function solution(n) {
     let pizzaCount = 1;
     while(true) {
         if(pizzaCount * 6 % n === 0) {
-            return pizzaCount;
+            break;
         }
         pizzaCount = pizzaCount + 1;
     }
+    return pizzaCount
 }
 
 // 1명일때, 1판 6조각씩 ok
@@ -16,4 +17,6 @@ function solution(n) {
 // 4판 4조각남음 x, 5판 6조각씩 ok
 
 // 17명 일때, 1~16 x , 17판 이어야 ok
+// 피자 판수를 하나씩 늘리려면 .. 힘드니까 반복문 쓰자
+// 사람 수와 피자 판수가 같으면 무조건 조건에 만족하니까 무한 반복해도 되겠다.
 // 피자 판수를 하나하나씩 늘린다음 조건에 만족하는 피자판수를 뽑으면 되겠네
