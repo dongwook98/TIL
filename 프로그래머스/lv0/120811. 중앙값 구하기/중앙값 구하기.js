13e94e832bfe05952893fec9cdf50752a425f736
dongwookkang <119502777+dongwook98@users.noreply.github.com>
2023-03-12 17:53:09 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
@@ -1,42 +1,36 @@
 function solution(array) {
-   
     
-    let k=0;
-    let newArr = [];
-    
-    while(k < array.length) {
-        let minNum = 1000;
-        
-        // 1-1. 최소값구하기
-        let i=0;
-        while(i<array.length) {
-            if(minNum > array[i]){
-                minNum = array[i];
+    let arrCnt = 0;
+    let newArray = [];
+    while(arrCnt < array.length) {
+        let cnt = 0;
+        let minNumber = 1000;
+        while(cnt < array.length) {
+            if(minNumber > array[cnt]) {
+                minNumber = array[cnt];
             }
-            i++
-        }
+            cnt++;
+        } // minNumber 찾음.
+        newArray.push(minNumber);
 
-        // 1-2. 찾으면 새배열에 최소값넣기
-        newArr.push(minNum);
-
-        // 1-3. 원래 배열에서 찾은값 지우기
-        let j=0;
-        while(j<array.length) {
-            if(minNum === array[j]) {
-                array[j] = 1000;
+        let cnt2 = 0;
+        while(cnt2 < array.length) {
+            if(array[cnt2] === minNumber) {
+                array[cnt2] = 1000;
                 break;
             }
-            j++
+            cnt2++;
         }
-            k++
-        }
-    console.log(newArr)
-    return newArr[Math.floor(array.length / 2)]
+        arrCnt++;
+    } // 정렬 끝
+    
+    let middleIndex = Math.floor(array.length / 2);
+    return newArray[middleIndex];
 }
-// [9, -1, 0]
-// 1.배열정렬해줌
-// 1-1. 배열 중 가장 작은 값을 찾는다.
+// 1. 정렬
+// 1-1. 배열 중 가장 작은값을 찾는다.
 // 1-2. 찾으면 새 배열에 넣는다.
 // 1-3. 원래 배열에 찾은 값은 지운다.
 // 1-4. 만약 원래배열 길이만큼 반복했으면 끝낸다. 아니면 1-1로 돌아간다.
-// 2.가운데 값 꺼내기
+
+// 2. 가운데 값 꺼내기
